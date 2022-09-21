bc6a9e53284730b974e0ee7d7c7c1f4e1cdd407f
NafisProttoy <113944134+NafisProttoy@users.noreply.github.com>
2022-09-21 16:10:26 -0400

    completed coding the website
    Completed coding the website but still personal information need to be added and updated

--- nafisprottoy/index.js
+++ nafisprottoy/index.js
@@ -17,3 +17,36 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+
+// SIDE MENU ANIMATION LOGIC
+
+let sideMenu = document.getElementById("sidemenu");
+
+function openMenu(){
+    sideMenu.style.right="0";
+}
+
+function closeMenu(){
+    sideMenu.style.right="-200px";
+}
+
+
+// ---------------------Contact Form Script Logic-----------------------
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyEriG0rxAhT0-WAKkPLO_vDiaT5X_CBU0DCbnc-TZetw-CUN-0URyevU_w0iQPqXkJRA/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg");
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message Sent Succesfully!"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        }, 5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
